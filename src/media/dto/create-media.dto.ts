398c32c5e585b9cd20d9ac70bdcd670edd28f02c
boydmeyer <boydmeyer@hotmail.com>
2022-05-12 11:22:49 +0200

    Class validation

--- src/media/dto/create-media.dto.ts
+++ src/media/dto/create-media.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateMediaDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 255)
   postId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 255)
   mediaId: string;
 }
